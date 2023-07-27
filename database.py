8c2542c03370ba7a1e9cec87ec4a5d258523f6a5
Vihaan Philip <viha0001@student.monash.edu>
2023-07-27 11:11:37 +0800

    querying the database

--- database.py
+++ database.py
@@ -8,12 +8,10 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor
 c = conn.cursor()
 
+
+#################
 # Create a table
-c.execute("""CREATE TABLE customers (
-            first_name DATATYPE,
-            last_name DATATYPE,
-            email DATATYPE
-          )""")
+#################
 
 #SQLite Datatypes:
 # NULL
@@ -21,3 +19,55 @@ c.execute("""CREATE TABLE customers (
 # REAL
 # TEXT
 # BLOB
+
+# c.execute("""CREATE TABLE customers (
+#             first_name text,
+#             last_name text,
+#             email text
+#           )""")
+
+###############################
+# Insert one record into table
+###############################
+
+# c.execute("INSERT INTO customers VALUES ('Mary', 'Brown', 'mary@codemy.com')")
+
+# print("Command executed successfully...")
+
+#################################
+# Insert many records into table
+#################################
+
+# many_customers = [
+#                     ('Wes', 'Brown', 'wes@brown.com'),
+#                     ('Steph', 'Kueg', 'steph@mail.com'),
+#                     ('Dan', 'Pas', 'dan@mail.com'),
+#                 ]
+
+# c.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+
+# print("Command executed successfully...")
+
+
+#################################
+# Query the database
+#################################
+
+c.execute("SELECT * FROM customers")
+# print(c.fetchone()[0])
+# c.fetchmany(3)
+
+items = c.fetchall()
+
+print("NAME" + "\t\tEMAIL")
+print("-----" + "\t\t-----")
+for item in items:
+    print(item[0] + " " + item[1] + "\t" + item[2])
+
+# print("Command executed successfully...")
+
+# Commit our command
+conn.commit()
+
+#Close our connection
+conn.close()
